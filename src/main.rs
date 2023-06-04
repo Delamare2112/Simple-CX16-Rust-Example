c6c6f66f770cd1d8fe2aadab30ef2ed103ea42a3
Trevor Berninger <baladashman@protonmail.com>
2023-06-03 21:39:06 -0700

    a face

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,32 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[rustfmt::skip]
+static FACE: [u8; 64] = [
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 0, 0, 1, 1,
+    1, 0, 1, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 0, 1, 0, 1,
+    1, 0, 0, 1, 1, 0, 0, 1,
+    1, 1, 0, 0, 0, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1];
+
 #[start]
 fn _main(_argc: isize, _argv: *const *const u8) -> isize {
-    println!("Hello {}!", 6502);
+    println!("Hello {}!", 6502); // You'll only see this if the mode set fails.
     unsafe {
         cx16_k_screen_mode_set(128);
+        println!("HELLO FROM A NEW MODE!"); // You'll only see this if console_init fails.
+        cx16_k_console_init(10, 10, 320 - 20, 240 - 20);
+        let message = b"\x92Hello Commander X16!";
+        for c in message {
+            cx16_k_console_put_char(*c, 0);
+        }
+        let _ = cx16_k_console_get_char();
+        // Transmute to ignore *const as *mut (this function _should_ be *const)
+        cx16_k_console_put_image(core::mem::transmute(FACE.as_ptr()), 8, 8);
     }
-    println!("HELLO FROM A NEW MODE!");
     0
 }
 
